d3df7245da8aa6f9ce4e17c806b2875df15f399c
Richard Khonan <richardkhonan0797@gmail.com>
2022-03-13 20:49:30 +0700

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Getting Started with Create React App
+# Setup
 
+1. ```yarn install```.
+2. Create ```.env``` in root directory.
+3. Add variable to store backend url ```REACT_APP_BASE_URL=http://localhost:5000```.
+4. Run ```npm start```
--- src/components/email_contents.js
+++ src/components/email_contents.js
@@ -47,7 +47,7 @@ export default function EmailContents(props) {
                                 </td>
                             </tr>
                         ))
-                    })
+                    }
                 </tbody>
             </Table>
         </>
